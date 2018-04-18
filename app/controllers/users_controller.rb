6d251536cedda44cf51dcd6649b6f1a89b966251
laura-campbell <campbell.laura.elizabeth@gmail.com>
2018-04-18 15:55:15 -0400

    changed datapase for user profile signup page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,6 +24,6 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:username, :password, :password_confirmation)
+      params.require(:user).permit(:username, :password, :password_confirmation, :first_name, :last_name, :email, :bio, :image_url)
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -55,8 +55,13 @@ ActiveRecord::Schema.define(version: 20180418172150) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
     t.string "username"
     t.string "password_digest"
+    t.string "image_url"
+    t.text "bio"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
