f354a9c1b9ed9e7251563e99ff393d3d7f1c79a9
laura-campbell <campbell.laura.elizabeth@gmail.com>
2018-04-18 23:26:57 -0400

    added some more show pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,12 +11,8 @@ class UsersController < ApplicationController
 
     def new
       @user = User.new
-<<<<<<< HEAD
       @interest = Interest.new
       @interests = Interest.all
-=======
-  
->>>>>>> stylingApp
     end
 
     def create
