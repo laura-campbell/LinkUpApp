2a8f48dc1ff4d474bdf36ff535515a33fdac3ebe
laura-campbell <campbell.laura.elizabeth@gmail.com>
2018-04-20 11:10:59 -0400

    fixed some cool shit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,24 +15,41 @@ class UsersController < ApplicationController
 
     def new
       @user = User.new
-      @interest = Interest.new
       @interests = Interest.all
     end
 
     def create
       @user = User.create(user_params)
       if @user.valid?
-        user_params[:i_ids].each do |i|
+        params[:user][:interest_ids].each do |i|
           @user_interest = UserInterest.create(user_id: @user.id, interest_id: i)
         end
+        redirect_to @user
       else
         redirect_to signup_path
       end
     end
 
+    def edit
+      @user = User.find(params[:id])
+      @interests = Interest.all
+    end
+
+    def update
+      @user = User.find(params[:id])
+      @user = User.update(user_params)
+      redirect_to user_path(@user)
+    end
+
+    def destroy
+      @user = User.find(params[:id])
+      @user.destroy
+      redirect_to signup_path
+    end
+
   private
 
     def user_params
-      params.require(:user).permit(:username, :password, :password_confirmation, :first_name, :last_name, :email, :bio, :image_url, :i_ids)
+      params.require(:user).permit(:username, :password, :password_confirmation, :first_name, :last_name, :email, :bio, :image_url, :interest_ids)
     end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,11 @@ class User < ApplicationRecord
   has_many :guests
   has_many :guest_link_ups, through: :guests, source: :link_up
 
+  validates :username, uniqueness: true
+  validates :email, uniqueness: true 
+
   def guest
-    
+
   end
 
 end
